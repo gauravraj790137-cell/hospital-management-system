0e56c1a89299cb5dbe52eb7082dc4c535b0e0590
gauravraj790137 <gauravraj790137@gmail.com>
2026-08-23 21:43:59 +0530

    first commit

---
+++ src/constants.js
@@ -0,0 +1,1 @@
+DB_NAME = hospital_management
---
+++ src/server.js
@@ -0,0 +1,15 @@
+import app from "./app.js";
+import connectDB from "./config/db.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+
+const startserver = async()=>{
+    await connectDB();
+    const PORT = process.env.PORT || 8000;
+    app.listen(PORT, ()=>{
+        console.log(`server is running on port ${PORT}`)
+    });
+}
+
+startserver();
