aabcd67c110a0f53a5def9154dd6a88163d441c7
gauravraj790137 <gauravraj790137@gmail.com>
2026-08-27 16:06:48 +0530

    added department controllers

--- src/controllers/departments.controllers.js
+++ src/controllers/departments.controllers.js
@@ -21,4 +21,31 @@ import { departmentmodel } from "../models/departments.model";
         description,
         headOfDepartment,
     })
+    res.status(201).json(department);
  })
+
+
+ const getAllDepartments = asyncHandler(async (req,res)=>{
+    const departments = await departmentmodel.find();
+    res.status(200).json(departments);
+ })
+ const getDepartmentById = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+
+    const department = await departmentmodel.findById(id);
+
+    if (!department) {
+        res.status(404);
+        throw new Error("Department not found");
+    }
+
+    res.status(200).json(department);
+});
+
+
+ 
+
+
+
+
+
