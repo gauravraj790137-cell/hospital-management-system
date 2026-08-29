48141ba7e8f8acc2e94fb1e001894a99a6ea1798
gauravraj790137 <gauravraj790137@gmail.com>
2026-08-29 15:33:44 +0530

    fixed bugs

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -197,7 +197,7 @@ const changeCurrentPassword = asyncHandler(async(req, res) => {
 
     
 
-    const user = await User.findById(req.user?._id)
+    const user = await user.findById(req.user?._id)
     const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
 
     if (!isPasswordCorrect) {
