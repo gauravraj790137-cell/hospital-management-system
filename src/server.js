e3658bcad4877423bec8a296b61c37be67a0f967
gauravraj790137 <gauravraj790137@gmail.com>
2026-08-30 14:46:51 +0530

    added doc and user routes to express

--- src/server.js
+++ src/server.js
@@ -2,6 +2,9 @@ import app from "./app.js";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
 dotenv.config();
+import userRoutes from "./routes/userRoutes.js";
+import doctorRoutes from "./routes/doctorRoutes.js";
+
 
 
 const startserver = async()=>{
@@ -16,3 +19,5 @@ startserver();
 
 import departmentroutes from "./routes/department.routes.js";   
 app.use("/api/departments", departmentroutes);
+app.use("/api/users", userRoutes);
+app.use("/api/doctors", doctorRoutes);
