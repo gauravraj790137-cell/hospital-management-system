09e2786a23418753504261d84c4617efb67a6329
gauravraj790137 <gauravraj790137@gmail.com>
2026-09-06 21:38:21 +0530

    changed pateints routes

--- src/routes/patients.routes.js
+++ src/routes/patients.routes.js
@@ -1,25 +1,49 @@
 import express from "express";
-
 import {
-    createPatient,
-    getAllPatients,
-    getPatientById,
-    updatePatient,
-    deletePatient
+  createPatient,
+  getAllPatients,
+  getPatientById,
+  updatePatient,
+  deletePatient
 } from "../controllers/patientController.js";
-
 import { verifyJWT } from "../middlewares/verifyJWT.js";
+import { authorizeRoles } from "../middlewares/authorizeRoles.js";
 
 const router = express.Router();
 
-router.post("/", verifyJWT, createPatient);
+router.post(
+  "/",
+  verifyJWT,
+  authorizeRoles("admin", "doctor"),
+  createPatient
+);
 
-router.get("/", verifyJWT, getAllPatients);
+router.get(
+  "/",
+  verifyJWT,
+  authorizeRoles("admin", "doctor"),
+  getAllPatients
+);
 
-router.get("/:id", verifyJWT, getPatientById);
+router.get(
+  "/:id",
+  verifyJWT,
+  authorizeRoles("admin", "doctor", "patient"),
+  getPatientById
+);
 
-router.patch("/:id",  updatePatient);
+router.patch(
+  "/:id",
+  verifyJWT,
+  authorizeRoles("admin", "doctor"),
+  updatePatient
+);
 
-router.delete("/:id", verifyJWT, deletePatient);
+router.delete(
+  "/:id",
+  verifyJWT,
+  authorizeRoles("admin"),
+  deletePatient
+);
 
 export default router;
