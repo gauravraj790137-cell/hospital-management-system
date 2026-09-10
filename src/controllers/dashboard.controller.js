c1c1ede4f14ebcad0c86efa49cf20e9698f132fd
gauravraj790137 <gauravraj790137@gmail.com>
2026-09-10 17:14:00 +0530

    added dashboard controllers

---
+++ src/controllers/dashboard.controller.js
@@ -0,0 +1,7 @@
+
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { usermodel } from "../models/user.model.js";
+import { doctormodel } from "../models/doctor.model.js";
+import { patientmodel } from "../models/patient.model.js";
+import { departmentmodel } from "../models/department.model.js";
+import { appointmentmodel } from "../models/appointment.model.js";
