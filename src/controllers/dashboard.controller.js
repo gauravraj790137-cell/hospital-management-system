632161f48bda2c7ea788ccaac12c9b76edfbc2ff
gauravraj790137 <gauravraj790137@gmail.com>
2026-09-10 17:15:13 +0530

    added controllers to dashboard

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -5,3 +5,24 @@ import { doctormodel } from "../models/doctor.model.js";
 import { patientmodel } from "../models/patient.model.js";
 import { departmentmodel } from "../models/department.model.js";
 import { appointmentmodel } from "../models/appointment.model.js";
+
+
+
+export const getDashboardOverview = asyncHandler(async (req, res) => {
+  const totalUsers = await usermodel.countDocuments();
+  const totalDoctors = await doctormodel.countDocuments();
+  const totalPatients = await patientmodel.countDocuments();
+  const totalDepartments = await departmentmodel.countDocuments();
+  const totalAppointments = await appointmentmodel.countDocuments();
+
+  res.status(200).json({
+    success: true,
+    data: {
+      totalUsers,
+      totalDoctors,
+      totalPatients,
+      totalDepartments,
+      totalAppointments
+    }
+  });
+});
