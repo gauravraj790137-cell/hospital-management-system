7d218daa26b3551c53240f28a3b2beb7226055ca
gauravraj790137 <gauravraj790137@gmail.com>
2026-09-11 21:32:22 +0530

    added dashboard routes

---
+++ src/routes/dashboard.routes.js
@@ -0,0 +1,25 @@
+import express from "express";
+import {
+  getDashboardOverview,
+  getAppointmentsByDepartment
+} from "../controllers/dashboardController.js";
+import { verifyJWT } from "../middlewares/verifyJWT.js";
+import { authorizeRoles } from "../middlewares/authorizeRoles.js";
+
+const router = express.Router();
+
+router.get(
+  "/overview",
+  verifyJWT,
+  authorizeRoles("admin"),
+  getDashboardOverview
+);
+
+router.get(
+  "/appointments-by-department",
+  verifyJWT,
+  authorizeRoles("admin"),
+  getAppointmentsByDepartment
+);
+
+export default router;
